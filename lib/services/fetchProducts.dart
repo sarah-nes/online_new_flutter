c325a06a9db040bf76f97718065fb13206f62cb8
sarah-nes <bleninfinity@gmail.com>
2021-10-10 21:26:34 +0300

    update body

--- lib/services/fetchProducts.dart
+++ lib/services/fetchProducts.dart
@@ -1,20 +1,26 @@
-// ignore: file_names
 import 'dart:convert';
 
+import 'package:furniture_app/models/Product.dart';
 import 'package:http/http.dart' as http;
-import 'package:online_new_flutter/models/product.dart';
 
-//fetch product from API
-Future<List<Product>> fetchproducts() async {
+// Fetch our Products from API
+Future<List<Product>> fetchProducts() async {
   const String apiUrl =
-      "https://5f210aagdaa42f001666535e.mockapi.io/api/products";
-  final response = await https.get(apiUrl);
+      "https://5f210aa9daa42f001666535e.mockapi.io/api/products";
+
+  final response = await http.get(apiUrl);
+
   if (response.statusCode == 200) {
-    List<product> products = (json.decode(response.body) as List)
-        .map((data) => product.fromJson(data))
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    List<Product> products = (json.decode(response.body) as List)
+        .map((data) => Product.fromJson(data))
         .toList();
+    // Return list of products
     return products;
   } else {
-    throw Exception('failed to load');
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load');
   }
 }
